3082ca166905ca6d79698e581ffd18bbb6c05d87
Froghut <abomb@abomb.de>
2019-03-14 16:02:26 +0100

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.jfsoftware.de/timerle104.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'timerle*'
+
+  checksum      = 'A3E5E62551B2213CEDBE76E522B9EE1B269203DAFF0E62C868AF5894DE045326'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
